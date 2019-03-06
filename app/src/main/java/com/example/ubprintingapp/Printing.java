aed3245b9dedd765d1a4b969411be79c97c2ceb1
Vladjaye <irshansk@yahoo.com>
2019-03-05 20:42:14 -0500

    commit setup

---
+++ app/src/main/java/com/example/ubprintingapp/Printing.java
@@ -0,0 +1,38 @@
+package com.example.ubprintingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Printing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_printing);
+
+
+    }
+
+
+
+    //Author: Vladyslav Iakusevych
+    public int calculatetime(int jobcount ){
+        int jobsqueue = jobcount;
+        int maptime = 0; //trave time to library from Google Maps API
+        int result = jobsqueue * 10 + maptime;
+        //10 seconds per page
+        //20 seconds per job
+        //TODO: for sprint #2 - improve algorithm by counting printing time individually for every page among all jobs
+
+
+        return result;
+    }
+
+    //Author: Vladyslav Iakusevych
+    public int generatequeue(){
+        int random  = (int)(Math.random() * 10 +1);
+        return random;
+
+        //TODO: Rewrite it for individual pages algorithm
+    }
+}
---
+++ app/src/test/java/com/example/ubprintingapp/BasicAlgoTest.java
@@ -0,0 +1,17 @@
+package com.example.ubprintingapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class BasicAlgoTest {
+    @Test
+    public void algotimecheck() {   //simple assertTrue test to check if algo provides correct printing time based on queue and eta to library.
+        Printing printactivity = new Printing();
+
+
+
+        assertEquals(100, printactivity.calculatetime(10) );
+    }
+}
