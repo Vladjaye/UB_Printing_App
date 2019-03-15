e782f610a738e4dd555bdbaad13d633232441c1f
mariaani <mariaani@buffalo.edu>
2019-03-15 04:35:20 -0400

    Rewrote the map 1)	I created a new API from Google Maps Platform (used free trial) 2)	Created a new class “Parser” which was getting all routes and gave result as a list of lists of latitude and longitude. 3)	On the Maps class I was getting url directions, and downloading data from a google map. 4)	I created URL which contains current user’s location, destination location (Capen library), sensor, and the new API. 5)	I created a method which was downloading (using http I was connected to url, I read the data from it, and the result was a string with a data) 6)	I created AsyncTask which was taking and placing data from URL (server and client side) and placing it. 7)	Created a new class PTask, which is helping to place locations in right format (JSONObject) (helped to parse), moreover in that class I was taking all direction points between 2 locations, and created blue lines. After I created a polyline on the map. 8)	I added working “show my location” button, which is on right corner. 9)	I fixed some bugs (on some phones map did not work before) Now direction is created with a blue working polyline.

---
+++ app/src/main/java/com/example/ubprintingapp/Parser.java
@@ -0,0 +1,112 @@
+package com.example.ubprintingapp;
+
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//need for map
+
+    public class Parser {
+
+        //by using JsonObject I'm getting all routes and result will be a list <list> with lat and long
+
+        public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+
+            List<List<HashMap<String, String>>> routes = new ArrayList<>() ;
+
+            JSONArray jsonArraySteps;
+            JSONArray jsonArrayRoutes;
+            JSONArray jsonArrayLegs;
+
+
+            try {
+
+                jsonArrayRoutes = jObject.getJSONArray("routes");
+
+                //for routes
+                for(int a=0;a<jsonArrayRoutes.length();a++){
+                    jsonArrayLegs = ( (JSONObject)jsonArrayRoutes.get(a)).getJSONArray("legs");
+                    List path = new ArrayList<>();
+
+                    //for legs
+                    for(int b=0;b<jsonArrayLegs.length();b++){
+                        jsonArraySteps = ( (JSONObject)jsonArrayLegs.get(b)).getJSONArray("steps");
+
+                        //steps
+                        for(int c=0; c<jsonArraySteps.length(); c++){
+                            String polyline = "";
+                            polyline = (String)((JSONObject)((JSONObject)jsonArraySteps.get(c)).get("polyline")).get("points");
+                            List<LatLng> list = decoder(polyline);
+
+                            //for point
+                            for(int d=0;d<list.size();d++){
+
+                                HashMap<String, String> hm = new HashMap<>();
+                                hm.put("lat", Double.toString((list.get(d)).latitude) );
+                                hm.put("lng", Double.toString((list.get(d)).longitude) );
+                                path.add(hm);
+                            }
+                        }
+                        routes.add(path);
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }catch (Exception e){
+            }
+
+
+            return routes;
+        }
+
+
+        private List<LatLng> decoder(String stringCode) {
+
+            List<LatLng> Listpoly = new ArrayList<>();
+            int idx = 0, len = stringCode.length();
+            int lat = 0, lng = 0;
+
+            while (idx < len) {
+                int b, shift = 0, result = 0;
+                do {
+                    b = stringCode.charAt(idx++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lat += dlat;
+
+                shift = 0;
+                result = 0;
+                do {
+                    b = stringCode.charAt(idx++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lng += dlng;
+
+                LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+                Listpoly.add(p);
+            }
+
+            return Listpoly;
+        }
+    }
+
+
+
+
+
+
+
+
