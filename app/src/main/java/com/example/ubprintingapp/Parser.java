5a22109144354e675302b390d5fe30b9e13471a1
mariaani <mariaani@buffalo.edu>
2019-03-29 12:06:11 -0400

    Distance between the library and user
    In parser class I'm getting distance from JSON Array.
    I'm adding the distance to my path.
    
    In the "Maps" class I'm getting distance from my Array List, which I created before.
    Checking if it is a correct result.

--- app/src/main/java/com/example/ubprintingapp/Parser.java
+++ app/src/main/java/com/example/ubprintingapp/Parser.java
@@ -15,122 +15,116 @@ import java.util.List;
 
 //need for map
 
-    public class Parser {
+public class Parser {
 
 
-        //by using JsonObject I'm getting all routes and result will be a list <list> with lat and long
+    //by using JsonObject I'm getting all routes and result will be a list <list> with lat and long
 
-        public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+    public List<List<HashMap<String,String>>> parse(JSONObject jObject){
 
-            List<List<HashMap<String, String>>> routes = new ArrayList<>() ;
+        List<List<HashMap<String, String>>> routes = new ArrayList<>() ;
 
-            JSONArray jsonArraySteps;
-            JSONArray jsonArrayRoutes;
-            JSONArray jsonArrayLegs;
-            JSONObject jsonObjectDistance;
-            JSONObject jsonObjectDuration;
-
-
-            try {
-
-                jsonArrayRoutes = jObject.getJSONArray("routes");
-
-                //for routes
-                for(int a=0;a<jsonArrayRoutes.length();a++){
-                    jsonArrayLegs = ( (JSONObject)jsonArrayRoutes.get(a)).getJSONArray("legs");
-                    List path = new ArrayList<>();
-
-                    //for legs
-                    for(int b=0;b<jsonArrayLegs.length();b++){
+        JSONArray jsonArraySteps;
+        JSONArray jsonArrayRoutes;
+        JSONArray jsonArrayLegs;
+        JSONObject jsonObjectDistance;
+     
 
 
+        try {
 
-                        //Get Distance
-                         jsonObjectDistance = ((JSONObject) jsonArrayLegs.get(b)).getJSONObject("distance");
-                        HashMap<String, String> hashMapDistance = new HashMap<String, String>();
-                        hashMapDistance.put("distance", jsonObjectDistance.getString("text"));
+            jsonArrayRoutes = jObject.getJSONArray("routes");
+
+            //for routes
+            for(int a=0;a<jsonArrayRoutes.length();a++){
+                jsonArrayLegs = ( (JSONObject)jsonArrayRoutes.get(a)).getJSONArray("legs");
+                List path = new ArrayList<>();
+
+                //for legs
+                for(int b=0;b<jsonArrayLegs.length();b++){
+
+
+
+                    //Get Distance
+                    jsonObjectDistance = ((JSONObject) jsonArrayLegs.get(b)).getJSONObject("distance");
+                    HashMap<String, String> hashMapDistance = new HashMap<String, String>();
+                    hashMapDistance.put("distance", jsonObjectDistance.getString("text"));
 
 
 //I add the distance to the path
-                        path.add(hashMapDistance);
+                    path.add(hashMapDistance);
 
 
 
 
-                        //end new
+                    //end new
 
 
 
-                        jsonArraySteps = ( (JSONObject)jsonArrayLegs.get(b)).getJSONArray("steps");
+                    jsonArraySteps = ( (JSONObject)jsonArrayLegs.get(b)).getJSONArray("steps");
 
-                        //steps
-                        for(int c=0; c<jsonArraySteps.length(); c++){
-                            String polyline = "";
-                            polyline = (String)((JSONObject)((JSONObject)jsonArraySteps.get(c)).get("polyline")).get("points");
-                            List<LatLng> list = decoder(polyline);
+                    //steps
+                    for(int c=0; c<jsonArraySteps.length(); c++){
+                        String polyline = "";
+                        polyline = (String)((JSONObject)((JSONObject)jsonArraySteps.get(c)).get("polyline")).get("points");
+                        List<LatLng> list = decoder(polyline);
 
-                            //for point
-                            for(int d=0;d<list.size();d++){
+                        //for point
+                        for(int d=0;d<list.size();d++){
 
-                                HashMap<String, String> hm = new HashMap<>();
-                                hm.put("lat", Double.toString((list.get(d)).latitude) );
-                                hm.put("lng", Double.toString((list.get(d)).longitude) );
-                                path.add(hm);
-                            }
+                            HashMap<String, String> hm = new HashMap<>();
+                            hm.put("lat", Double.toString((list.get(d)).latitude) );
+                            hm.put("lng", Double.toString((list.get(d)).longitude) );
+                            path.add(hm);
                         }
-                        routes.add(path);
                     }
+                    routes.add(path);
                 }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }catch (Exception e){
             }
 
-
-            return routes;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (Exception e){
         }
 
 
-        private List<LatLng> decoder(String stringCode) {
-
-            List<LatLng> Listpoly = new ArrayList<>();
-            int idx = 0, len = stringCode.length();
-            int lat = 0, lng = 0;
-
-            while (idx < len) {
-                int b, shift = 0, result = 0;
-                do {
-                    b = stringCode.charAt(idx++) - 63;
-                    result |= (b & 0x1f) << shift;
-                    shift += 5;
-                } while (b >= 0x20);
-                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-                lat += dlat;
-
-                shift = 0;
-                result = 0;
-                do {
-                    b = stringCode.charAt(idx++) - 63;
-                    result |= (b & 0x1f) << shift;
-                    shift += 5;
-                } while (b >= 0x20);
-                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-                lng += dlng;
-
-                LatLng p = new LatLng((((double) lat / 1E5)),
-                        (((double) lng / 1E5)));
-                Listpoly.add(p);
-            }
-
-            return Listpoly;
-        }
+        return routes;
     }
 
 
+    private List<LatLng> decoder(String stringCode) {
 
+        List<LatLng> Listpoly = new ArrayList<>();
+        int idx = 0, len = stringCode.length();
+        int lat = 0, lng = 0;
 
+        while (idx < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = stringCode.charAt(idx++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
 
+            shift = 0;
+            result = 0;
+            do {
+                b = stringCode.charAt(idx++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
 
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            Listpoly.add(p);
+        }
+
+        return Listpoly;
+    }
+}
 
 
