8a0aa63d27b8ac766838ed9c7e1c6b2a802dc8c3
mariaani <mariaani@buffalo.edu>
2019-04-26 22:49:52 -0400

    Started implementing Maps as a service for ETA algo.

---
+++ app/src/main/java/ServiceMap.java
@@ -0,0 +1,13 @@
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ServiceMap extends Service {
+ //   @androidx.annotation.Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
+
+
