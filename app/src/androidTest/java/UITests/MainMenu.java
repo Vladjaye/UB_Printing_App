9096c0a09f8496e7bdce308ae55816634dee111d
mariaani <mariaani@buffalo.edu>
2019-05-10 11:19:03 -0400

    1.Added a check, which will tell if the map was displayed and the direction is to the right library. 2.Added UI test to check if “Ink change” sometimes appears 3.Added UI test to check if PC List appears 4.Added UI test to see if the total time will be showing 5.Added UI test to check if the user will be able to see the position of the library, after clicking the button “show on map”

--- app/src/androidTest/java/UITests/MainMenu.java
+++ app/src/androidTest/java/UITests/MainMenu.java
@@ -14,7 +14,10 @@ import androidx.test.rule.ActivityTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 
 @LargeTest
 
@@ -33,31 +36,95 @@ public class MainMenu extends Activity {
 
     @Test
     public void press_ShowOnMap_Capen() {
-        // press the button "Show on map".
+
       //  com.example.ubprintingapp.Fragment_capen
        //         .perform(click);
-        onView(withId(R.id.capbutton)).perform(click());
 
-       //We will see the map, if it works.
+        onView(withId(R.id.PrintingButton)).perform(click());
+        //clicking in main menu upper on the upper button (with printer icon)
+
+        onView(withId(R.id.capbutton)).perform(click()).check(matches(isDisplayed()));
+        // press the button "Show on map".
+
+        //We will see the map and direction from the user location to capen library
+        //it will check if the map is displayed.
     }
 
     @Test
     public void press_ShowOnMap_Lockwood() {
-        // press the button "Show on map".
+
         //  com.example.ubprintingapp.Fragment_lockwood
         //         .perform(click);
-        onView(withId(R.id.lockbutton)).perform(click());
 
-        //We will see the map, if it works.
+        onView(withId(R.id.PrintingButton)).perform(click());
+        //clicking in main menu upper on the upper button (with printer icon)
+
+        onView(withId(R.id.lockbutton)).perform(click()).check(matches(isDisplayed()));
+        // press the button "Show on map".
+
+
+        //it will check if the map is displayed.
+        //We will see the map and direction from the user location to lockwood
     }
 
     @Test
     public void press_ShowOnMap_Music() {
-        // press the button "Show on map".
+
         //  com.example.ubprintingapp.Fragment_music
         //         .perform(click);
-        onView(withId(R.id.musicbutton)).perform(click());
 
-        //We will see the map, if it works.
+        onView(withId(R.id.PrintingButton)).perform(click());
+        //clicking in main menu upper on the upper button (with printer icon)
+
+
+        onView(withId(R.id.musicbutton)).perform(click()).check(matches(isDisplayed()));
+        // press the button "Show on map".
+
+        //it will check if the map is displayed.
+        //We will see the map and direction from the user location to music library
+
     }
+
+
+    @Test
+    public void CheckingIfInkChangeInProgressAppears() {
+        // we will see "Ink Change In Progress" if it will appear
+        //the chance to see that is 1/10.
+
+        onView(withId(R.id.PrintingButton)).perform(click()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void CheckingListOfPC() {
+
+        //it will check if the PC list is displayed.
+        onView(withId(R.id.PCListButton)).perform(click()).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void CheckingTotalTime() {
+        // we will see checking time on the display
+
+        onView(withId(R.id.PrintingButton)).perform(click()).check(matches(isDisplayed()));
+    }
+    
+
+
+    //Checking if the user will be able to see the position
+    // of the library, after clicking the button “show on map”
+    @Test
+    public void CheckingPosition() {
+
+        onView(withId(R.id.PrintingButton)).perform(click());
+        //clicking in main menu upper on the upper button (with printer icon)
+
+        onView(withId(R.id.capbutton)).perform(click()).check(matches(isDisplayed()));
+        // press the button "Show on map".
+
+        //We will see if the position will be showing on the capen library
+    }
+
+
+
 }
